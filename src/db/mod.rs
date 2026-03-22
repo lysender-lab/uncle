f22621fc79613547982d19b08e50a4d11f44acd2
Lysender <lysender@darkstar>
2026-03-22 16:07:53 +0800

    fix: cleanup unused modules

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -1,19 +1,9 @@
-mod app;
 mod db;
 mod image;
 mod image_prompt;
 mod job;
-mod oauth_code;
-mod org;
-mod org_app;
-mod org_member;
-mod password;
-mod superuser;
 mod turso_decode;
 mod turso_params;
-mod user;
 
-mod error;
-
+pub use crate::{Error, Result};
 pub use db::{DbMapper, create_db_mapper};
-pub use error::{Error, Result};
